9288ebec0847a298c2e9c8458115df2c8799984c
Harsh Patidar <hpatidar457@gmail.com>
2025-03-04 18:55:51 +0530

    Updated Linear Search Program

--- Arrays/Easy/Linear Search.py
+++ Arrays/Easy/Linear Search.py
@@ -1,1 +1,16 @@
-print("Linear Search")
+"""
+************
+Optimal Code
+********************************************************************************
+1. Iterate through array in a linear fashion and return index or value.
+********************************************************************************
+"""
+
+def linearSearch(nums):
+
+    n = len(nums)
+    for i in range(n):
+        if nums[i] == target:
+            return i
+
+    return -1
