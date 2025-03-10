32c3a097381cce7e79586d18d24bef05f08ef940
Harsh Patidar <hpatidar457@gmail.com>
2025-03-10 20:44:53 +0530

    Added more Programs

--- Binary Search/BS on Answers/Koko Eating Bananas.py
+++ Binary Search/BS on Answers/Koko Eating Bananas.py
@@ -1,16 +1,42 @@
+import math
 """
 **************
 Brute Approach
 **********************************************
+1. Take out the maximum value from the array, which will be max bananas, can be consumed
+    over the array/piles
+2. Run a loop i from 1 -> maximumPileBanana + 1 calculated from above step
+3. Declare/Take two variables, r pointer for moving across the array/piles
+    and a timeConsumed variable to calculate hours be taken eating that number of bananas
+    piles[r]/(i) --> i is the number of bananas can be taken.
+4. Once timeConsumed is calculated, check if it is <=h as per the question, if it is then
+    return the value
 
-
-TC : O(n)
+TC : O(max(array) * n)
 SC : O(1)
 **********************************************
 """
 
 def kokoEatingBananas(piles, h):
-    pass
+
+    # Maximum bananas that can be consumed
+    maxBananaPile = max(piles)
+    n = len(piles)
+
+    # Iterate over each banana
+    for i in range(1, maxBananaPile + 1):
+        r = 0
+        totalHoursConsumed = 0
+
+        while r < n:
+            # If koko is on a pile and if he used to eat i number of bananas at given pile
+            # It would take piles[r]/i hours to finish that pile.
+            totalHoursConsumed += math.ceil(piles[r] / i)
+            r += 1
+
+        # If the time taken is lesser than h then this would be by answer
+        if totalHoursConsumed <= h:
+            return i
 
 
 
@@ -19,41 +45,51 @@ def kokoEatingBananas(piles, h):
 Optimal Approach
 **********************************************
 1. The same BS approach will be followed
-2. Initially keep the pointer low = 1 and high = n or n//2
+2. Initially keep the pointer low = 1 and high = maxEle of array
 3. Calculate the mid element using (low + high) // 2
 4. While loop till low <= high
-5. Check
-    - mid * mid == n : return mid
-    - mid * mid < n : low = mid + 1
-    - else : high = mid - 1
-6. Once done return high ( Do it on a paper you would understand why high will be returned )
+5. Calculate the hours required for that mid element using a calculateHours func, If :
+    - It is within the stipulated time means we can get a smaller element on left so h = m-1
+    - Else l = mid + 1
+6. Once done return low ( Do it on a paper you would understand why low will be returned )
 
-TC : O(log n)
+TC : O(n * log2(maxEleOfArray))
 SC : O(1)
 **********************************************
 """
 
-def kokoEatingBananas(n):
-
+def kokoEatingBananas(piles, n):
+    maxBananaPile = max(piles)
     low = 1
-    high = n
+    high = maxBananaPile
 
+    # Function to calculate totalHours for the middle element.
+    def calculateHours(piles, mid):
+
+        totalHoursConsumed = 0
+        for num in piles:
+            totalHoursConsumed += math.ceil(num / mid)
+
+        return totalHoursConsumed
+
+    # Perform BS on the range
     while low <= high:
+
         mid = (low + high) // 2
 
-        # If middle element itself equals n then return
-        if mid * mid == n:
-            return mid
+        # Calculate totalHours for midElement
+        totalHours = calculateHours(piles, mid)
 
-        # If it is less, means need a greater value, shift low forward
-        elif mid * mid < n:
-            low = mid + 1
-
-        # Else shift high backward
-        else:
+        # If it is within the stipulated time, let's check on left if there is any smaller
+        # number.
+        if totalHours <= h:
             high = mid - 1
 
-    return high
+        # Else on right side
+        else:
+            low = mid + 1
+
+    return low
 
 # Input
 piles = [3,6,7,11]
