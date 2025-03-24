faa80914cbc3b86a44299c330e86c0c7b4f8f197
Harsh Patidar <hpatidar457@gmail.com>
2025-03-24 13:51:42 +0530

    Added more Programs

--- Linked List/Medium/Detect a loop in LL.py
+++ Linked List/Medium/Detect a loop in LL.py
@@ -1,6 +1,45 @@
 """
 **************
-Approach
+Brute Approach
+************************************************************
+1. Initialize a dictionary to hold the visited nodes
+2. Traverse through the LL until it reaches end, and check if the node is in dictionary or
+    not, if not then insert the node else return True because it was already there, and
+    it is revisited again.
+3. Once traversal is done, return False. In case if LL is not in a loop.
+
+TC : O(n)
+SC : O(n)
+"""
+
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+
+def loopDetect(self, head):
+
+    mapp = dict()
+
+    currentNode = head
+
+    while None != currentNode:
+
+        if currentNode not in mapp:
+            mapp[currentNode] = 1
+        else:
+            return True
+
+        currentNode = currentNode.next
+
+    return False
+
+
+
+"""
+**************
+Optimal Approach
 ************************************************************
 1. The approach we will be using is, slow and fast pointers.
 2. Traverse in LL until the fast pointer reaches last Node or out of LL.
@@ -11,13 +50,6 @@ Approach
 TC : O(n)
 SC : O(1)
 """
-
-
-class Node:
-    def __init__(self, value):
-        self.data = value
-        self.next = None
-
 
 def loopDetect(self, head):
 
